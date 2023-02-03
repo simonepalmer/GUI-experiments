811b530d2be9ee545fcb0294b29d554d7ffc3577
Simon <118159732+simonepalmer@users.noreply.github.com>
2023-02-03 08:36:27 +0100

    Changed error handling
    I gave up on the error button. Why it didn't work is beyond my capabilities at this stage.
    
    Instead I spent time on doing a check if pipewire is even installed and making different errors depending on if it is or not which seems like a waste of time since no one will probably ever use this app except for me!

--- GUI-pipewire-spls-control/pipewire-control.py
+++ GUI-pipewire-spls-control/pipewire-control.py
@@ -1,5 +1,5 @@
-import gi # to use GTK
-import os # to run shell commands
+import gi
+import os
 
 gi.require_version("Gtk", "3.0") 
 from gi.repository import Gtk
@@ -11,23 +11,31 @@ class Control:
         self.buffer = buffer
         self.sample = sample
 
-    def apply_buffer_sample(self):
-        try:
-            # os.system(f'this_command_creates_an_error {buffer}')
-            os.system(f'pw-metadata -n settings 0 clock.force-quantum {self.buffer}')
-            os.system(f'pw-metadata -n settings 0 clock.force-rate {self.sample}')
-        except Exception as e:
-            self.show_error_window("An error occurred while applying the buffer and sample", str(e))
+    """Checking if Pipewire is installed and apply settings"""
+    def apply_settings(self):
+        # Check if pipewire is installed and show error if not.
+        if os.popen('which pipewire').read() == '/usr/bin/pipewire\n':
+            # then try to catch other errors if it is but not working
+            try:
+                os.system(f'pw-metadata -n settings 0 clock.force-quantum {self.buffer}')
+                os.system(f'pw-metadata -n settings 0 clock.force-rate {self.sample}')
+            except Exception as error:
+                message = "Pipewire is installed but can't set seleceted settings"
+                self.show_error_window(message, str(error))
+        else:
+            message = "Pipewire can't be found, use the command: 'which pipewire' to see if it is installed"
+            self.show_error_window(message, "If not, install Pipewire and try again")
 
-    def show_error_window(self, title, message):
+    """Showing error popup"""
+    def show_error_window(self, message, error):
         builder = Gtk.Builder()
         builder.add_from_file("pipewire-control.glade")
-        
         window_error = builder.get_object("window_error")
-        window_error.set_title("Pipewire error")
-        
+
         label_error = builder.get_object("label_error")
-        label_error.set_text("Could not set the selected settings, make sure you have pipewire installed.")
+        label_error.set_text(message)
+        label_hint = builder.get_object("label_hint")
+        label_hint.set_text(error)
 
         window_error.show_all()
 
@@ -45,20 +53,13 @@ class ControlWindow:
         self.window.connect("destroy", Gtk.main_quit)
 
     """apply & close buttons"""
-    # This button is not working, In fact, it doesn't
-    # print the message either but the other two buttons
-    # do. I'm not sure what I am doing wrong :(
-    def on_close_error_clicked(self, clicked):
-        # print("IS SOMETHING HAPPENING?")
-        window_error.hide()
-
     def on_close_clicked(self, clicked):
         # print("closing")
         Gtk.main_quit()
 
     def on_apply_clicked(self, clicked):
         # print("appying")
-        self.control.apply_buffer_sample()
+        self.control.apply_settings()
     
     """Buffer size radio buttons"""
     def on_radio64_toggled(self, toggled):
