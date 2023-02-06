907986c7093273a42c490c2ece71f2d6eb7af4a6
Simon <118159732+simonepalmer@users.noreply.github.com>
2023-02-06 08:43:14 +0100

    Fixed get_current_settings()
    I found a bug in get_current_settings where they both showed '0' after a reboot which was because the output I used for it was the currently FORCED setting and after a reboot there is no forced and it uses a DEFAULT setting instead. I also changed the way of getting the output to something I hope if less prone to breaking if pipewire changes the way it lists it's settings
    
    Lastly. Added some comments with questions I have for anyone seeing this that would be willing to help

--- GUI-pipewire-spls-control/pipewire-control.py
+++ GUI-pipewire-spls-control/pipewire-control.py
@@ -7,9 +7,9 @@ from gi.repository import Gtk
 class Control:
     """Control class for business logic."""
     
-    def __init__(self, buffer=64, sample=48000):
+    def __init__(self, buffer=64, samples=48000):
         self.buffer = buffer
-        self.sample = sample
+        self.samples = samples
         self.control_window = None
 
     """If Pipewire is installed then apply settings, else show error"""
@@ -18,8 +18,14 @@ class Control:
             try:
                 os.system(f'pw-metadata -n settings 0 clock.force-quantum {self.buffer}')
                 self.control_window.label_buffer_settings.set_text(f"Sample size: {str(self.buffer)} samples")
-                os.system(f'pw-metadata -n settings 0 clock.force-rate {self.sample}')
-                self.control_window.label_sample_settings.set_text(f"Sample rate: {str(self.sample)} kHz")
+                os.system(f'pw-metadata -n settings 0 clock.force-rate {self.samples}')
+                self.control_window.label_sample_settings.set_text(f"Sample rate: {str(self.samples)} kHz")
+
+                # I though about using the 'get_current_settings()' method to set the number here aswell
+                # but since the button methods already sets the buffer and samples to intergers for the 
+                # command to set the settings it seems stupid to not just use the same variables to for
+                # the label
+
             except Exception as error:
                 message = "Pipewire is installed but can't set seleceted settings"
                 self.control_window.on_error(message, str(error))
@@ -31,9 +37,31 @@ class Control:
     def get_current_settings(self):
         current_settings = os.popen('pw-metadata -n settings').read()
         settings_list = current_settings.split("'")
-        buffer = settings_list[-10]
-        samples = settings_list[-4]
+
+        buffer_i = settings_list.index('clock.force-quantum')
+        buffer = settings_list[buffer_i+2]
+        samples_i = settings_list.index('clock.force-rate')
+        samples = settings_list[samples_i+2]
+
+        if buffer == '0':
+            buffer_i = settings_list.index('clock.quantum')
+            buffer = settings_list[buffer_i+2]
+        if samples == '0':
+            samples_i = settings_list.index('clock.rate')
+            samples = settings_list[samples_i+2]
+
         return buffer, samples
+
+        # I found 2 problems in the previous version of this method.
+
+        # 1st: If pipewire changes the order of their settings of in other ways change the format
+        # of the output the settings_list[-4] could be something completely different, however, the
+        # new way should be safer to not break since it's looking for the name of the settings it
+        # wants the value for and then taking that index + 2 (+1 would be 'value:') 
+
+        # 2nd: Pipewire has a "default" and a 'forced' settings of these values. To make it always
+        # show the correct current setting it needs to check if the forced value is '0', which it 
+        # will be after a reboot, and then take the default value instead.
 
 class ControlWindow:
     """ControlWindow class for GUI logic."""
@@ -80,12 +108,12 @@ class ControlWindow:
 
     """Samaple rate radio buttons"""
     def on_radio48_toggled(self, toggled):
-        self.control.sample = 48000
+        self.control.samples = 48000
 
     def on_radio44_toggled(self, toggled):
-        self.control.sample = 44100
+        self.control.samples = 44100
 
-    """Showing error popup"""
+    """Showing error message"""
     def on_error(self, message, error):
         builder = Gtk.Builder()
         builder.add_from_file("pipewire-control.glade")
@@ -97,6 +125,18 @@ class ControlWindow:
         label_hint.set_text(error)
 
         window_error.show_all()
+
+        # I'm not sure why every other method here need to use self.x
+        # to work but this one can't have it, I think I have a lacking
+        # understanding of how classes work which prevents me from 
+        # figuring this out. Any input or explaination would be very helpful 
+
+        # Also, I would very much like to have a button in this window
+        # aswell connected to on_close_clicked() to close the entire app,
+        # because there is not reason to keep it running if pipewire is
+        # not installed. However, I probably spent 2 hours last week trying
+        # to get a button to work without any progress, it was just doing
+        # nothing so I skipped it for now
 
 if __name__ == "__main__":
     control = Control()
