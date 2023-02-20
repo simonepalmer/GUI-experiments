d00019118bc0c008cf800403cc56060877c27ccd
Simon <118159732+simonepalmer@users.noreply.github.com>
2023-02-20 08:58:32 +0100

    Finally "works"
    It's now properly separated into classes for handling the GUI and handling the logic
    
    I have Fixed formatting in the output so it's the same as the input
    
    I addressed special characters and make sure to convert (the ones I have come across) into decimal form so they can be multiplied.
    
    Fixing a slight quirk that makes the program add .0 decimals to some conversions. for example 50% of 300 g would be 150.0 g. But no longer
    
    I should add that I have not yet tested with a lot of different inputs so I don't know of all the cases that it could be tripped up. After, free user input has a lot of potential to be parsed unexpectedly :D

--- GUI-scale-recipie/scale-recipe.py
+++ GUI-scale-recipie/scale-recipe.py
@@ -4,97 +4,113 @@ import gi # Gtk
 gi.require_version("Gtk", "3.0") 
 from gi.repository import Gtk
 
-# I am aware that best practices is to keep GUI in a seperate
-# class but I'm a dumb dumb so I just want to figure out how
-# to do the actuall task first. Then I can make it look neat.
-
-""" GUI stuff """
-def build_gui():
-
-    builder = Gtk.Builder()
-    builder.add_from_file("scale.ui")
-
-    window = builder.get_object("window")
-    window.show_all()
-    window.connect("destroy", Gtk.main_quit)
-
-    Gtk.main()
-
-""" Buttons things """
-def on_close_clicked(clicked):
-    Gtk.main_quit()
-
-def on_apply_clicked(clicked):
-    pass
-
-""" Functions to do the business """
-def update_numbers_in_string(input_string, ratio):
-
-    # I have noticed recipies have characters like "½ & ¾" in them
-    # sometimes so I should probably build in something that checks
-    # for these to replace with 0.5 and 0.75. Not decided if doing
-    # in it the same bit as the rest or if trying to sanitize the
-    # string beforehand
-
-    numbers = re.findall(r'\d+\.\d+|\d+', input_string)
-
-    updated_numbers = []
-    for num in numbers:
-        if '.' in num:
-            updated_numbers.append(str(float(num) * ratio))
-        else:
-            updated_numbers.append(str(int(num) * ratio))
-
-    print(numbers)
-    print(updated_numbers)
-
-    # I'm keeping both versions right now until I can come along
-    # with further testing to know which one, if any, is better
-
-    """ Split with re """
-    words = re.findall(r'\b\w+\b|\d+\.\d+|\d+', input_string)
-
-    output_words = []
-    for word in words:
-        if any(char.isdigit() for char in word):
-            for i in range(len(numbers)):
-                if numbers[i] in word:
-                    word = word.replace(numbers[i], updated_numbers[i])
-                    break
-        output_words.append(word)
-
-    """ Split with python split """
-    # words = input_string.split()
-    #
-    # output_words = []
-    # for word in words:
-    #     if any(char.isdigit() for char in word):
-    #         for i in range(len(numbers)):
-    #             if numbers[i] in word:
-    #                 word = word.replace(numbers[i], updated_numbers[i])
-    #                 break
-    #     output_words.append(word)
-
-    output_string = " ".join(output_words)
-    return output_string
+class Control:
+    """Control class for business logic."""
     
-def take_input_string():
-    # Will have a textbox to paste the recipe into
-    # until then I will have this to test
-    input_string = "a 1  b 2  c 3  d 4  e 1.5  f 2.5  g 0.5  h 0.25"
-    return input_string
+    def __init__(self, ratio=0.5):
+        self.ratio = ratio
+        self.control_window = None
 
-def give_output(input_string, output_string):
-    print("done")
+    def update_numbers_in_string(self, input_string):
 
-def main():
-    build_gui()
+        # Convert special characters to their actual numbers (So that they can be multiplied)
+        input_string = input_string.replace('¼', '0.25').replace('½', '0.5').replace('¾', '0.75')
 
-    # These will all be run by on_apply_clicked() later
-    input_string = take_input_string()
-    ratio = 1.5 # Will be decided by radio buttons
-    output_string = update_numbers_in_string(input_string, ratio)
-    give_output(input_string, output_string)
+        # Split into lines to be able to keep format in the end
+        output_lines = []
+        input_lines = input_string.split("\n")
+        
+        # Find all numbers
+        for input_line in input_lines:
+            numbers = re.findall(r'\d+\.\d+|\d+', input_line)
+
+            # Make a new list of all the numbers multiplied by the selected ratio
+            updated_numbers = []
+            for num in numbers:
+                if '.' in num:
+                    updated_numbers.append(str(float(num) * self.ratio))
+                else:
+                    updated_numbers.append(str(int(num) * self.ratio))
+
+            # Split the input_line into words
+            words = input_line.split()
+
+            output_words = []
+            for word in words:
+                # Check if the word contains any digits
+                if any(char.isdigit() for char in word):
+                    # Loop over each updated number to replace the original number with the updated one
+                    for i in range(len(numbers)):
+                        # Replace the original number with the updated number of the same index
+                        if numbers[i] in word:
+                            # Fix unwanted decimals, for example 150.0 g
+                            if '.' in updated_numbers[i]:
+                                updated_numbers[i] = updated_numbers[i].rstrip('0').rstrip('.')
+                            word = word.replace(numbers[i], updated_numbers[i])
+                            break
+
+                # Stiching everying up again!
+                output_words.append(word)
+
+            output_lines.append(" ".join(output_words))
+
+        output_string = "\n".join(output_lines)
+
+        return output_string
+
+class ControlWindow:
+    """ControlWindow class for GUI logic."""
+
+    def __init__(self, control):
+        self.control = control
+        self.control.control_window = self
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("scale.ui")
+        self.builder.connect_signals(self)
+
+        self.window = self.builder.get_object("window")
+        self.window.show_all()
+        self.window.connect("destroy", Gtk.main_quit)
+
+    """ Buttons things """
+    def on_close_clicked(self, clicked):
+        Gtk.main_quit()
+
+    def on_apply_clicked(self, clicked):
+        # Define and get text from input_box
+        input_buffer = self.builder.get_object("input_box").get_buffer()
+        input_string = input_buffer.get_text(input_buffer.get_start_iter(), input_buffer.get_end_iter(), True)
+
+        # Call the method to convert the input string
+        output_string = self.control.update_numbers_in_string(input_string)
+
+        # Define the output_box and set the text with the output_string
+        output_buffer = self.builder.get_object("output_box").get_buffer()
+        output_buffer.set_text(output_string)
+
+    """ Radio buttons """
+    def on_radio1_toggled(self, toggled):
+        self.control.ratio = 0.5
+
+    def on_radio2_toggled(self, toggled):
+        self.control.ratio = 0.75
+
+    def on_radio3_toggled(self, toggled):
+        self.control.ratio = 1
+
+    def on_radio4_toggled(self, toggled):
+        self.control.ratio = 1.25
+
+    def on_radio5_toggled(self, toggled):
+        self.control.ratio = 1.5
+
+    def on_radio6_toggled(self, toggled):
+        self.control.ratio = 1.75
+
+    def on_radio7_toggled(self, toggled):
+        self.control.ratio = 2
 
 if __name__ == "__main__":
-    main()
+    control = Control()
+    ControlWindow(control)
+    Gtk.main()
